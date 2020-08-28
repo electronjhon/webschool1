4260ff3b2df170f06ec6821630b5f14d412b1c8c
electronjhon <jvdesarrolloweb@gmail.com>
2020-08-28 08:49:33 -0500

    DataTable: Cursos --> conectada a API REST (Angular Material)

--- client/src/app/modules/components/cursos/cursos.component.ts
+++ client/src/app/modules/components/cursos/cursos.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ApiCursosService } from '../../services/api-cursos.service';
+import { CursoInterface } from '../../curso-interface';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, AfterViewInit {
+  ELEMENT_DATA: CursoInterface[];
+  displayedColumns: string[] = ['nombre', 'director'];
+  dataSource = new MatTableDataSource<CursoInterface>(this.ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(private apiCursosService: ApiCursosService) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    this.getListCursos();
   }
+
+  public getListCursos() {
+    let resp = this.apiCursosService.getAllCursos();
+    resp.subscribe(cursos => this.dataSource.data = cursos as CursoInterface[])
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
 
 }
